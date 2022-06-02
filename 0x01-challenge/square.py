6f92a2e98747fcd73b4a28a48d568fbbbc81ba5a
Luis Colon <luiscolon0426@gmail.com>
2022-06-02 14:22:24 -0400

    Added extra comment

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -28,7 +28,7 @@ class Square:
 
 
 if __name__ == "__main__":
-    """ Main """
+    """ Main jndojawifjaidjhfiwajfiuwae"""
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
